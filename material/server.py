4aa0e946ef4679edf54bbe19dd758178e3d0480a
Haiyun Lyu <112651748+ZoeyLLL@users.noreply.github.com>
2024-08-06 21:18:22 -0400

    Add files via upload

---
+++ material/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, send_from_directory
+from inference import answer
+
+app = Flask(__name__, static_folder="frontend", template_folder="frontend", static_url_path="")
+
+@app.route("/")
+def home_page():
+    return send_from_directory(app.static_folder, "index.html")
+
+@app.route("/query/<query>", methods=["GET"])
+def query(query):
+    answer_text = answer(query)
+    #answer_text = "This is a placeholder answer."
+    return {"answer": answer_text}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port = 7860)
